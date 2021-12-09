a8b79bdf26c9a705f56bfa3ae1b817648c925134
kirua <yo44prg@icloud.com>
2021-12-09 21:52:13 +0100

    feat: change url var for env var

--- api/scrapysneake.js
+++ api/scrapysneake.js
@@ -1,4 +1,6 @@
-const url = "https://scrapysneake.herokuapp.com/";
+import { API_URL } from "@env";
+
+const url = API_URL;
 
 export function getSneakerTrend() {
     let road = "sneakers/find/most_wanted";
